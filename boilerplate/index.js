05e04d2ed9818cbcfe8c18eb7d4665f968f85a2b
Sion Park <bgrrt3661@gmail.com>
2023-05-28 17:12:16 +0900

    week6

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,18 +1,49 @@
-const express = require('express')
-const { connect } = require('mongoose')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sion:password@cluster0.fsmqalx.mongodb.net/?retryWrites=true&w=majorityn', {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected')).catch(err => console.log(err))
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 // 몽고디비의 저장소에 접근을 해서 저장하고 그 정보를 불러와주는 웹 서비스
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  //body Parser를 통해 body에 담긴 정보를 가져옴
+  const user = new User(req.body);
+
+  //mongoDB 메서드, user모델에 저장
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
